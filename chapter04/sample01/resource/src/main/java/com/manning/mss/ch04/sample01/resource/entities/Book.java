4b9052b8d9be77c256ee00d38d9b530e7b28cd82
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2024-10-29 05:20:38 +0530

    Upgrade chapter 4 sampple 1 to boot 3.x and Java 21

--- chapter04/sample01/resource/src/main/java/com/manning/mss/ch04/sample01/resource/entities/Book.java
+++ chapter04/sample01/resource/src/main/java/com/manning/mss/ch04/sample01/resource/entities/Book.java
@@ -1,12 +1,15 @@
 package com.manning.mss.ch04.sample01.resource.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.Id;
 
 @Entity
 @Data

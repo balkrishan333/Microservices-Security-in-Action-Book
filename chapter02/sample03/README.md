8b7c2f417253f4427ff8276a6b682fb786c58cea
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2024-10-28 18:33:27 +0530

    Upgrade to boot 3.x, Java 21

--- chapter02/sample03/README.md
+++ chapter02/sample03/README.md
@@ -1,3 +1,10 @@
 ## Chapter 2: First steps in securing microservices (sample03)
 
-[https://github.com/microservices-security-in-action/samples/tree/master/chapter02/sample03](https://github.com/microservices-security-in-action/samples/tree/master/chapter02/sample03)
+- This is orders microservice running on port 8082
+- This is also resource server
+- Required authroization server to be running on port 8080
+  - Authorization server port can be changed in application.properties
+<!-- - This is integrated with API gateway also
+- Endpoints can be access either
+  - directly hitting this server or
+  - through API gateway running on port 8081 -->
--- chapter02/sample03/src/main/java/com/manning/mss/ch02/sample03/service/OrderProcessingService.java
+++ chapter02/sample03/src/main/java/com/manning/mss/ch02/sample03/service/OrderProcessingService.java
@@ -1,39 +1,33 @@
 package com.manning.mss.ch02.sample03.service;
 
+import com.manning.mss.ch02.sample03.exceptions.OrderNotFoundException;
+import com.manning.mss.ch02.sample03.orderentity.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.manning.mss.ch02.sample03.exceptions.OrderNotFoundException;
-import com.manning.mss.ch02.sample03.orderentity.Order;
-
 @EnableWebSecurity
 @RestController
 @RequestMapping("/orders")
-public class OrderProcessingService extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity
+public class OrderProcessingService {
 
-    private Map<String, Order> orders = new HashMap<>();
+    private final Map<String, Order> orders = new HashMap<>();
 
     @PostMapping
-    public ResponseEntity<Order> placeOrder(@RequestBody Order order) {
+    public ResponseEntity<Order> placeOrder(@AuthenticationPrincipal Jwt jwt, @RequestBody Order order) {
 
+        System.out.println("User : " + jwt.getSubject());
         System.out.println("Received Order For " + order.getItems().size() + " Items");
         order.getItems().forEach((lineItem) -> System.out.println("Item: " + lineItem.getItemCode() +
                 " Quantity: " + lineItem.getQuantity()));
@@ -41,34 +35,35 @@ public class OrderProcessingService extends WebSecurityConfigurerAdapter {
         String orderId = UUID.randomUUID().toString();
         order.setOrderId(orderId);
         orders.put(orderId, order);
-        return new ResponseEntity<Order>(order, HttpStatus.CREATED);
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Order> getOrder(@PathVariable String id) throws OrderNotFoundException {
+    public ResponseEntity<Order> getOrder(@AuthenticationPrincipal Jwt jwt, @PathVariable String id) throws OrderNotFoundException {
 
+        System.out.println("User : " + jwt.getSubject());
         if(orders.containsKey(id)){
-            return new ResponseEntity<Order>(orders.get(id), HttpStatus.OK);
-        }
-        else {
+            return new ResponseEntity<>(orders.get(id), HttpStatus.OK);
+        } else {
            throw new OrderNotFoundException();
         }
     }
 
-    @Bean
-    public ResourceServerTokenServices tokenServices() {
-        RemoteTokenServices tokenServices = new RemoteTokenServices();
-        tokenServices.setClientId("orderprocessingservice");
-        tokenServices.setClientSecret("orderprocessingservicesecret");
-        tokenServices.setCheckTokenEndpointUrl("http://localhost:8085/oauth/check_token");
-        return tokenServices;
-    }
+    @PostMapping("/with-role-security")
+    @PreAuthorize("hasAuthority('SCOPE_items.write')")
+    public ResponseEntity<Order> placeOrderWithRoleBasedSecurity(@RequestBody Order order) {
 
-    @Override
-    public AuthenticationManager authenticationManagerBean() {
-        OAuth2AuthenticationManager authenticationManager = new OAuth2AuthenticationManager();
-        authenticationManager.setTokenServices(tokenServices());
-        return authenticationManager;
-    }
+        var jwt = (Jwt)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
+        System.out.println("User : " + jwt.getSubject());
+        System.out.println("SecurityContextHolder = " + SecurityContextHolder.getContext().getAuthentication());
+        System.out.println("Received Order For " + order.getItems().size() + " Items");
+        order.getItems().forEach((lineItem) -> System.out.println("Item: " + lineItem.getItemCode() +
+                " Quantity: " + lineItem.getQuantity()));
+
+        String orderId = UUID.randomUUID().toString();
+        order.setOrderId(orderId);
+        orders.put(orderId, order);
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
 }

0135eba695b926a8129d2bde128f2e7186408e15
Nagpal, Balkrishan <balkrishan.nagpal@optum.com>
2024-10-29 19:37:58 +0530

    chapter 4 - sample 2- Upgraed to boot 3.x, Java 21, ng 18

--- chapter04/sample02/gateway/src/main/java/com/manning/mss/ch04/sample02/gateway/GatewayApplication.java
+++ chapter04/sample02/gateway/src/main/java/com/manning/mss/ch04/sample02/gateway/GatewayApplication.java
@@ -2,13 +2,7 @@ package com.manning.mss.ch04.sample02.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
@@ -16,15 +10,15 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/books/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST");
-            }
-        };
-    }
+    // @Bean
+    // public WebMvcConfigurer corsConfigurer() {
+    //     return new WebMvcConfigurer() {
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //             registry.addMapping("/books/**")
+    //                     .allowedOrigins("http://localhost:4200")
+    //                     .allowedMethods("GET", "POST");
+    //         }
+    //     };
+    // }
 
 }
